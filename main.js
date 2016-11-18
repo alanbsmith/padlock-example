eacfb4c14f1d19a16e99b360e5572017438ddb8c
Alan Smith <a.bax.smith@gmail.com>
2016-11-18 15:06:30 -0700

    initial setup

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('#login').submit((e) => {
+    e.preventDefault();
+    const password = $('#password').val();
+    // reset the value in the form
+    $('#password').val('');
+    // validate password
+    if (password !== 'mypassword') {
+      return alert('invalid password');
+    }
+    alert('welcome!');
+  });
+});
